0a200c211da370e1faea41273f1ec97736fb63a0
Edvardas Dlugauskas <edvardasdlugauskas@gmail.com>
2018-09-18 21:48:27 +0300

    Fix force_update.bat

--- dist/force_update.bat
+++ dist/force_update.bat
@@ -1,5 +1,6 @@
 :: Force-updates AutoRain.exe and rain.kv with the latest versions available on github
-powershell -noprofile -command ^
-Invoke-WebRequest -OutFile "rain.kv" -URI https://raw.githubusercontent.com/EdvardasDlugauskas/Auto-Rain/master/rain.kv;^
-Invoke-WebRequest -OutFile "AutoRain.exe" -URI https://github.com/EdvardasDlugauskas/Auto-Rain/raw/master/dist/AutoRain.exe;^
+powershell -noprofile -command
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;^
+Invoke-WebRequest -OutFile "rain.kv" -URI https://raw.githubusercontent.com/EdvardasDlugauskas/Auto-Rain/master/rain.kv;
+Invoke-WebRequest -OutFile "AutoRain.exe" -URI https://github.com/EdvardasDlugauskas/Auto-Rain/raw/master/dist/AutoRain.exe;
 Invoke-WebRequest -OutFile "force_update.bat" -URI https://github.com/EdvardasDlugauskas/Auto-Rain/raw/master/dist/force_update.bat;
